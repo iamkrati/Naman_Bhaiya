e207a394db879972f23c4c9f26b6439cc13ddcd5
Krati Goyal <gkrati04@gmail.com>
2022-10-19 10:30:19 +0530

    CB

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
-function typeconversion()
+// function typeconversion()
+// {
+//     let num=document.getElementById("userinput").ariaValueMax;
+//     console.log(+num,typeof +num);
+// }
+
+// sayHi();
+
+
+let sayHi=function()
 {
-    let num=document.getElementById("userinput").ariaValueMax;
-    console.log(+num,typeof +num);
+    console.log("Krati Goyal");
 }
+sayHi();
+
+let sum= (a,b) => a+b;
+
+
+console.log(sum(5,7));
+
+
+
+let num=15e5;
+num=1000_00_0;
+console.log(num.toString(16));
+
+
+let num2=12.47756;
+console.log(Math.round(num2*100)/100);
+
+console.log(num2.toFixed(2));
+console.log(num2.toFixed(2));
+
+console.log(parseFloat('13o2.45e'));
+
+
+// Arrays
+
---
+++ objects.js
@@ -0,0 +1,57 @@
+// let obj={
+//     name:'Krati',
+//     "hello1":1,
+//     'hello2':2,
+//     welcome:function(){
+//         console.log('welcome')
+//        }
+// };
+
+// console.log(obj.welcome());
+
+// let str="aabc bbca caab ddac"
+// let obj=
+// {
+// }
+// for(var i=0;i<str.length;i++)
+// {
+//     if(obj[str[i]]==undefined)
+//       obj[str[i]]=1;
+//     else
+//       obj[str[i]]+=1  ;
+// }
+// console.log(obj);
+// delete obj[" "]
+// console.log(obj);
+
+// Automatic garbage collector
+
+// 1.Global variable
+// 2.function still to execute
+// 
+
+
+let person={
+    firstname:'john',
+    age:24
+}
+let p1={
+    lastname:'doe',
+    __proto__:person  // tow times underscore
+}
+console.log(p1.age);
+
+let p2=
+ {
+    isAdult:true,
+    __proto__:p1
+}
+
+
+let ob={
+    "firstname":'krati',
+    age:24
+}
+console.log(ob);
+ob['lastname']='goyal'
+console.log(ob["lastname"]);
